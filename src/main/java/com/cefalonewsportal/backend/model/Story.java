caf8e550412d98768aa5d316e0635742fe24d1e3
Abrar <abrar.haq535@gmail.com>
2020-04-28 14:47:47 +0600

    Models Added

---
+++ src/main/java/com/cefalonewsportal/backend/model/Story.java
@@ -0,0 +1,78 @@
+package com.cefalonewsportal.backend.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "stories")
+public class Story {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private  Integer storyId;
+
+    private String title;
+    private String body;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(name = "published_date", nullable = false)
+//    @LastModifiedDate
+    private String publishedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY , optional = false)
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    public Integer getStoryId() {
+        return storyId;
+    }
+
+    public void setStoryId(Integer storyId) {
+        this.storyId = storyId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Story{" +
+                "storyId=" + storyId +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", publishedDate=" + publishedDate +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ src/main/java/com/cefalonewsportal/backend/model/User.java
@@ -0,0 +1,58 @@
+package com.cefalonewsportal.backend.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Integer userId;
+    private String userName;
+    private String fullName;
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
