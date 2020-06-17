a2dfa514f67331be72c02b4b1678d931542df7c3
Abrar <abrar.haq535@gmail.com>
2020-06-17 23:12:39 +0600

    File Upload init

---
+++ src/main/java/com/cefalonewsportal/backend/controller/FileSystemController.java
@@ -0,0 +1,13 @@
+package com.cefalonewsportal.backend.controller;
+
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileSystemController {
+fgfg
+
+
+}
