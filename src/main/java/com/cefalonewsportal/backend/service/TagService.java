a78aa75c51393c39580fe0fcad5d27181b731300
Abrar <abrar.haq535@gmail.com>
2020-06-17 13:36:05 +0600

    Added Tagging to Stories (completed)

--- src/main/java/com/cefalonewsportal/backend/service/TagService.java
+++ src/main/java/com/cefalonewsportal/backend/service/TagService.java
@@ -1,16 +1,10 @@
 package com.cefalonewsportal.backend.service;
-
-import com.cefalonewsportal.backend.model.Story;
 import com.cefalonewsportal.backend.model.Tag;
 import com.cefalonewsportal.backend.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-
 import static java.util.Comparator.comparingInt;
 
 @Service
